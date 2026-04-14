02d8b1f341dcc54d06c0340544b8760164b6a4f5
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2026-04-14 09:14:44 +0200

    off-policy control added

---
+++ chap5/blackjack/off_policy.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+from typing import List, Tuple
+
+from env import BlackjackEnv, BlackjackState
+from episods import EpisodeGenerator, simple_policy
+
+
+class OffPolicy:
+    def __init__(self) -> None:
+        pass
