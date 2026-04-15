2c1e75cf9da90b78f8ba0060d17093fd7a0f5b14
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2026-04-15 10:42:38 +0200

    process module initiated

--- chap6/mrp_project/mrp.py
+++ chap6/mrp_project/mrp.py
@@ -1,12 +1,21 @@
+import random
+
+
 class RandomWalkMRP:
     def __init__(self, n_states=100):
         self.n_states = n_states
         self.states = list(range(self.n_states))
         self.state = None
+        self.ACTIONS = ["l", "r"]
 
     def reset(self):
         self.state = self.n_states // 2
         return self.state
 
     def step(self):
-        pass
+        action = random.choice(self.ACTIONS)
+
+        if action == "l":
+            self.state -= 1
+        else:
+            self.state += 1
